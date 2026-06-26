24fe8dacaaf2202d398e1dd7f308d85f517934be
Ankit Sharma <ankit0525252@gmail.com>
2026-06-26 13:51:50 +0530

    added shadcn components

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
     <div>
       <h1>Hi there</h1>
-      <Button variant="outline">Click me!</Button>
+      <Card></Card>
     </div>
   );
 }
